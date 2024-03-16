a2377caf0c37d5e14e4013f290fc0679dbb5ec6a
Andrew Jones <thebinarysearchtree@gmail.com>
2024-03-16 19:15:19 +1000

    updates

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 import express from 'express';
+import { join } from 'path';
+import { writeFile } from 'fs/promises';
 
 const app = express();
+
+let i = 1;
 
 app.use('/', express.static('./'));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/contact', async (req, res) => {
-  console.log(req.body);
+app.get('/quietone', (req, res) => {
+  const path = join(import.meta.dirname, 'quietone.html');
+  res.sendFile(path);
+});
+
+app.get('/quietone/contact', (req, res) => {
+  const path = join(import.meta.dirname, 'contact.html');
+  res.sendFile(path);
+});
+
+app.post('/quietone/submit', async (req, res) => {
+  await writeFile(`message${i}.json`, JSON.stringify(req.body));
+  i++;
+  return res.redirect('/quietone?submit=true');
 });
 
 const server = app.listen(3000);
